615b5becbd5a231a8f4759614e5efd5eb1908d65
Soroush <s.ghodrat@gmail.com>
2024-11-08 08:38:24 +0100

    Add function to generate alphabet based on language

--- src/util/helper.ts
+++ src/util/helper.ts
@@ -5,3 +5,13 @@ export const goToTop = () => {
 export const randonColor = () => {
   return "#" + Math.floor(Math.random() * 16777215).toString(16);
 };
+
+export const generateAlphabet = (language: "english" | "norwegian"): string[] => {
+  const baseAlphabet = Array.from({ length: 26 }, (_, i) => String.fromCharCode(i + 65));
+
+  if (language === "norwegian") {
+    return baseAlphabet.concat(["Æ", "Ø", "Å"]);
+  }
+
+  return baseAlphabet;
+};
