4996011d7801b317e90ba883bab124f89d114a83
Soroush <s.ghodrat@gmail.com>
2024-11-08 08:42:16 +0100

    Enhance DataTable components for language-specific functionality and improve search filtering

--- src/components/DataTableAlphabetsBox.tsx
+++ src/components/DataTableAlphabetsBox.tsx
@@ -1,26 +1,38 @@
 import { Box, ListItemButton, ListItemText, Typography } from "@mui/material";
+import { generateAlphabet } from "../util/helper";
+import { useEffect, useMemo, useState } from "react";
+import { useLanguage } from "../context/LanguageContext";
 
 const DataTableAlphabetsBox = () => {
-  const letters = Array.from({ length: 26 }, (_, i) =>
-    String.fromCharCode(i + 65),
-  ).concat(["Æ", "Ø", "Å"]);
+  const englishLetters = useMemo(() => generateAlphabet("english"), []);
+  const norwegianLetters = useMemo(() => generateAlphabet("norwegian"), []);
+
+  const { language } = useLanguage();
+  const [currentLanguageLetters, setCurrentLanguageLetters] = useState<string[]>([]);
+
+  useEffect(() => {
+    // Set default language to Norwegian if no language is set or if it's set to Norwegian
+    if (language === "Norsk" || !language) {
+      setCurrentLanguageLetters(norwegianLetters);
+    } else if (language === "English") {
+      setCurrentLanguageLetters(englishLetters);
+    }
+  }, [language, englishLetters, norwegianLetters]);
+
   return (
     <Box display="flex" flexDirection="column" alignItems="center">
-      <Typography
-        align="center"
-        m={5}
-        variant="h3"
-        sx={{ textTransform: "capitalize" }}
-      >
-        mest brukte norske verbene
+      <Typography align="center" m={5} variant="h3" sx={{ textTransform: "capitalize" }}>
+        {language === "Norsk" ? "vanlige norske verbene" : "common english verbs"}
       </Typography>
 
       <Box sx={{ borderRadius: "10px", backgroundColor: "#d7e8f4" }} p={2}>
         <Typography pb={2} variant="h6" textAlign="center">
-          Trykk på et alfabet for å hoppe til gruppen
+          {language === "Norsk"
+            ? "Trykk på et alfabet for å hoppe til gruppen"
+            : "Press a letter to jump to the group"}
         </Typography>
         <Box display="flex" flexWrap="wrap" gap={2}>
-          {letters.map((letter) => (
+          {currentLanguageLetters.map((letter) => (
             <ListItemButton key={letter} component="a" href={`#${letter}`}>
               <ListItemText primary={letter} />
             </ListItemButton>
--- src/components/DataTableSearchBox.tsx
+++ src/components/DataTableSearchBox.tsx
@@ -1,34 +1,44 @@
-import {
-  Box,
-  TextField,
-  InputAdornment,
-  IconButton,
-  Zoom,
-} from "@mui/material";
+import { Box, TextField, InputAdornment, IconButton, Zoom } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import { useState } from "react";
-import { verbs } from "../data/no"; // import your Verb type
+import { no_verbs } from "../data/no"; // import your Verb type
+import { en_verbs } from "../data/en"; // import your Verb type
 import { HtmlTooltip } from "../styled/Styled";
+import { useLanguage } from "../context/LanguageContext";
 
+type Verb = {
+  infinitiv?: string;
+  presens?: string;
+  preteritum?: string;
+  perfektum?: string;
+  imperative?: string;
+  meaning?: string;
+};
 interface DataTableSearchBoxProps {
   onSearch: (filteredVerbs: Verb[], value: string) => void;
 }
 
 // Extract TextField into its own function
 const SearchField: React.FC<DataTableSearchBoxProps> = ({ onSearch }) => {
+  const { language } = useLanguage();
   const [searchValue, setSearchValue] = useState("");
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
     setSearchValue(value);
 
-    const filtered = verbs.filter((verb) => {
-      return Object.values(verb).some((val) =>
-        val.toLowerCase().includes(value.toLowerCase()),
-      );
-    });
-
-    console.log("filtered", filtered);
+    const filtered =
+      language === "Norsk"
+        ? no_verbs.filter((verb) => {
+            return Object.values(verb).some((val) =>
+              val.toLowerCase().includes(value.toLowerCase()),
+            );
+          })
+        : en_verbs.filter((verb) => {
+            return Object.values(verb).some((val) =>
+              val.toLowerCase().includes(value.toLowerCase()),
+            );
+          });
 
     onSearch(filtered, value);
   };
@@ -50,11 +60,7 @@ const SearchField: React.FC<DataTableSearchBoxProps> = ({ onSearch }) => {
       onChange={handleInputChange}
       InputProps={{
         endAdornment: (
-          <HtmlTooltip
-            title="Fjern søk "
-            placement="left"
-            TransitionComponent={Zoom}
-          >
+          <HtmlTooltip title="Fjern søk " placement="left" TransitionComponent={Zoom}>
             <InputAdornment position="end">
               <IconButton edge="end" onClick={handleClearInput}>
                 <CloseIcon />
